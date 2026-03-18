0c50dbdcb28f3cfdd00a2e350c071b9ba648e183
joshpadernal29 <josh.padernal29@gmail.com>
2026-03-19 01:31:43 +0800

    fixed logic error for admin login

--- sit-in system/action/login_logic.php
+++ sit-in system/action/login_logic.php
@@ -8,9 +8,11 @@ $adminPass = "admin123";
 
 if (isset($_POST['user_login'])) {
     // if admin login check credentials
-    if ($_POST['user_id'] && $_POST['user_password']) {
+    if ($_POST['user_id'] === $adminID && $_POST['user_password'] === $adminPass) {
         header("Location:  ../admin_module/adminDashboard.php");
     }
 
+    // db get data logic here...
     // user login check credentials
+    // if ($_POST['user_id'] === )
 }
