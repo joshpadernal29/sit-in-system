6bb4cd592e0d45699fab70ef2856675e8298956d
joshpadernal29 <josh.padernal29@gmail.com>
2026-03-20 02:15:31 +0800

    added the user login logic which will retrieve student id in the db and compare hashed password before logging in

--- sit-in system/action/login_logic.php
+++ sit-in system/action/login_logic.php
@@ -14,12 +14,30 @@ if (isset($_POST['user_login'])) {
     // if admin login check credentials
     if ($_POST['user_id'] === $adminID && $_POST['user_password'] === $adminPass) {
         $_SESSION['user_id'] = $_POST['user_id'];
-        $_SESSION['user_password'] = $_POST['user_password'];
         header("Location:  ../admin_module/adminDashboard.php");
         die(); // exit
     }
 
-    // db get data logic here...
     // user login check credentials
-    // if ($_POST['user_id'] === )
+    $userId = trim($_POST['user_id']);
+    $userPassword = trim($_POST['user_password']);
+
+    $sql = "SELECT student_id,password FROM students WHERE student_id = ? LIMIT 1";
+    $getData = mysqli_prepare($conn,$sql);
+    mysqli_stmt_bind_param($getData, 's', $userId);
+    // execute query
+    mysqli_stmt_execute($getData);
+    // get result from the query
+    $result = mysqli_stmt_get_result($getData);
+    // fetch student data from assoc array
+    $row = mysqli_fetch_assoc($result);
+    // if student data in db and compare password inputted to the hashed password from the db
+    if ($row && password_verify($userPassword,$row['password'])) {
+        $_SESSION['user_id'] = $row['student_id'];
+        header("Location: ../student_module/studentDashboard.php");
+        exit();
+    } else {
+        header("Location: ../login.php?error=1"); // temp message
+    }
+    
 }
