fd1017f3162940c8d2839282341d057e2cd1f0ca
joshpadernal29 <josh.padernal29@gmail.com>
2026-03-20 02:37:11 +0800

    added studentHeader for united student header

--- sit-in system/student_module/studentDashboard.php
+++ sit-in system/student_module/studentDashboard.php
@@ -3,13 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net">
     <title>dashboard</title>
 </head>
 <body>
-    <div class="container">
-        <div class="card">
-            <h1>dahsboard</h1>
-        </div>
-    </div>
+    <!--header-->
+        <?php include("../includes/studentHeader.php"); ?>
+    <!--header end-->
+
+    <!--main contend-->
+    <main>
+        <div class="container-fluid my-2">
+            <div class="card">
+                <h1>dahsboard</h1>
+            </div>
+        </div>  
+    </main>
+    <!--main contend end -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
