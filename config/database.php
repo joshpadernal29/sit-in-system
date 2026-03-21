872821b6cada887f0212a3f68109ffbbc4168735
joshpadernal29 <josh.padernal29@gmail.com>
2026-03-22 01:31:14 +0800

    changed db  port number

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $db_name = "sit_in_monitoring";
-$portNo = 3306;
+$portNo = 3307;
 
 // create connection 
 $conn = mysqli_connect($servername,$username,$password,$db_name,$portNo);
