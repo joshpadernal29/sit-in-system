2235ae20ab3e9ecaa19cc36db0ee25102dba134c
joshpadernal29 <josh.padernal29@gmail.com>
2026-03-26 00:47:16 +0800

    added a column for target students for annoucments

--- config/annoucements.sql
+++ config/annoucements.sql
@@ -3,9 +3,13 @@ CREATE TABLE announcements (
     title VARCHAR(255) NOT NULL,
     message TEXT NOT NULL,
     priority ENUM('general', 'urgent', 'academic') DEFAULT 'general',
+    target_audience ENUM('all', 'bsit', 'bscs') DEFAULT 'all' AFTER priority,
     is_active TINYINT(1) DEFAULT 1,
     date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- ALTER TABLE announcements 
 -- MODIFY COLUMN priority ENUM('general', 'urgent', 'academic') DEFAULT 'general';
+
+-- ALTER TABLE announcements 
+-- ADD COLUMN target_audience ENUM('all', 'bsit', 'bscs') DEFAULT 'all' AFTER priority;
