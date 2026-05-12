9807d616d0521a7e0d0de099e3745bd6d5ad0d33
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-13 02:57:42 +0800

    added function to get all testimonials

--- action/testimonials.php
+++ action/testimonials.php
@@ -1,7 +1,7 @@
 <?php
 include("../config/database.php");
 
-// submit testimonial
+// submit testimonial (student)
 $message = "";
 if (isset($_POST['submit_testimonial'])) {
     $getStudentPk = $_POST['student_pk'];
@@ -21,4 +21,43 @@ if (isset($_POST['submit_testimonial'])) {
         }
         mysqli_stmt_close($getData);
     }
+}
+
+// admin 
+// get testimonials from the dba_close
+// all (testimonoials)
+function getAllTestimonials($conn) {
+     $sql = "SELECT t.student_pk,
+                   t.content,
+                   t.rating,
+                   t.status,
+                   t.is_featured,
+                   t.created_at,
+                   CONCAT(st.firstname, ' ' ,st.lastname) AS fullname,
+                   st.student_id
+                   FROM testimonials t
+                   LEFT JOIN students st ON t.student_pk = st.id
+                   ORDER BY t.created_at DESC";
+    $getData = mysqli_prepare($conn,$sql);
+    if ($getData) {
+        mysqli_stmt_execute($getData);
+        $result = mysqli_stmt_get_result($getData);
+        mysqli_stmt_close($getData);
+
+        return $result;
+    }
+    return false;
+}
+
+
+// pending (testimonials)
+if(isset($_GET['pending_requests'])) {
+
+}
+
+
+
+// approved
+if(isset($_GET['approved_requests'])) {
+
 }
